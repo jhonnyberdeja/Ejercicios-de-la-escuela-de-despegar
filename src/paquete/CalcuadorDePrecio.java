4ffefadd84694471b0dd50e770e6fce9a613205c
jhonnyberdeja <jhonny.berdeja@despegar.com>
2016-02-22 21:44:25 -0300

    Relacionando CalculadorDePrecio con la interface Cliente, utilizando la flecha USE

--- src/paquete/CalcuadorDePrecio.java
+++ src/paquete/CalcuadorDePrecio.java
@@ -1,12 +1,16 @@
 package paquete;
 
 public class CalcuadorDePrecio {
-	
+/*	
 	public Double calcularPrecio(EmpleadoDeDespegar empleado,Double precioBase){
-		
 		return precioBase*empleado.getIndice(precioBase);
 	}
+	
 	public Double calcularPrecio(Pasajero pasajero,Double precioBase){
-		return precioBase*pasajero.getIndice(precioBase);
+		return precioBase*pasajero.getIndice(precioBase)+pasajero.getComision();
+	}
+*/
+	public Double calcularPrecio(Cliente cliente,Double precioBase){
+		return precioBase*cliente.getIndice(precioBase);
 	}
 }
--- src/paquete/Prueva.java
+++ src/paquete/Prueva.java
@@ -4,18 +4,20 @@ public class Prueva {
 
 	public static void main(String[] args) {
 		Double precioBase=1000.0;
+		
 		CalcuadorDePrecio calcuadorDePrecio=new CalcuadorDePrecio();
-		//PASAJERO
-		Pasajero pasajero=new Pasajero("Jhonny Beredja",2.3,1.2);
-		Double precioDePasajeParaPasajero=calcuadorDePrecio.calcularPrecio(pasajero,precioBase);
-		System.out.println("Nombre del pasajero : "+pasajero.getNombre());
-		System.out.println("Precio del pasajero : "+precioDePasajeParaPasajero);
 		
 		//EMPLEAD0O
 		EmpleadoDeDespegar empleado=new EmpleadoDeDespegar("Milton Ajhuacho",2.1);
 		Double precioDePasajeParaEmpleado=calcuadorDePrecio.calcularPrecio(empleado, precioBase);
 		System.out.println("Nombre del empleado : "+empleado.getNombre());
 		System.out.println("Precio del empleado : "+precioDePasajeParaEmpleado);
+		
+		//PASAJERO
+		Pasajero pasajero=new Pasajero("Jhonny Beredja",2.3,1.2);
+		Double precioDePasajeParaPasajero=calcuadorDePrecio.calcularPrecio(pasajero,precioBase);
+		System.out.println("Nombre del pasajero : "+pasajero.getNombre());
+		System.out.println("Precio del pasajero : "+precioDePasajeParaPasajero);
 
 		
 	}
